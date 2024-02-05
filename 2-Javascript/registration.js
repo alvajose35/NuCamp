f9257011e895b86c60ab753fb5a29b4bf6c636f0
Jose V <alvajose35@gmail.com>
2024-02-05 11:03:59 -0500

    Finish Workshop 3

--- 2-Javascript/registration.js
+++ 2-Javascript/registration.js
@@ -5,7 +5,7 @@ class Student {
 	}
 
 	getinfo() {
-		console.log(`The student ${this.name} email is ${this.email}`);
+		console.log(`The student ${this.name} email address is ${this.email}.`);
 	}
 }
 
@@ -17,28 +17,28 @@ class Bootcamp {
 	}
 
 	registerStudent(studentToRegister) {
-		// console.log(studentToRegister.name);
-		// console.log(studentToRegister.email);
-
+		
+		// Validate that student info is not missing before registering
 		if (studentToRegister.name === undefined || studentToRegister.email === undefined) {
-		// console.log("Inside first IF");
-			console.log(`Invalid name or email`);
+			console.log(`Invalid name or email.`);
 			return false;
 		}
 		else {
-		// console.log("Inside first else");
 
-
+			// Check to see if student is already registered
 			if (this.students.find(item => item.email == studentToRegister.email) != undefined) {
-				console.log(`Email is already registered. (find method)`);
+				console.log(`The email ${studentToRegister.email} is already registered.`);
 				return false;
 			}
+
 			// for (let std of this.students){
 			// 	if (studentToRegister.email === std.email) {
 			// 		console.log(`Email is already registered.`);
 			// 		return false;
 			// 	}
 			// }
+
+			// Register new student
 			this.students.push(studentToRegister);
 			console.log(`Successfully registered ${studentToRegister.name} in ${this.name} bootcamp.`);
 			return true;
@@ -46,13 +46,14 @@ class Bootcamp {
 	}
 
 	listStudents() {
+
+		// Check to see if list of students is empty
 		if (this.students.length === 0) {
 			console.log(`No students are registered to the ${this.name} bootcamp.`);
 			return false;
 		}
 		else {
 			console.log(`The students registered in ${this.name} are:`);
-
 			for(let stud of this.students) {
 				console.log(`Name: ${stud.name}  Email: ${stud.email}`);
 			}
@@ -60,22 +61,15 @@ class Bootcamp {
 		}
 	}
 
+// BONUS
+
 	getinfo() {
 		console.log(`This bootcamp name is "${this.name}" and it's level is "${this.level}".`);
 	}
 
 	removeStudent(email) {
-		// for (let student of this.students) {
-		// 	console.log(email);
-		// 	if (student.email === email) {
-		// 		console.log("here");
-		// 		let index =  this.students
-		// 		// this.students.splice(i, 1);
-		// 		console.log(`Successfully removed ${student.name} with the email ${student.email}.`);
-		// 		return true;
-		// 	}
-		// }
 
+		// Check to see if email exists and if so remove student
 		for (let x in this.students) {
 			if (this.students[x].email === email) {
 				console.log(`Successfully removed ${this.students[x].name} with the email ${this.students[x].email}.`);
@@ -83,14 +77,14 @@ class Bootcamp {
 				return true;
 			}
 		}
-
-
-		// if (this.students.email.includes(email))
 		
+		// No student with that email found
 		console.log(`ERROR: No student with the email ${email} found.`);
 	}
 
 	listStudentsTable() {
+
+		// Check to see if list of students is empty
 		if (this.students.length === 0) {
 			console.log(`No students are registered to the ${this.name} bootcamp.`);
 			return false;
@@ -103,14 +97,14 @@ class Bootcamp {
 	}
 }
 
-//TEST OF TASK 1 - Testing Student Class
+// TEST OF TASK 1 - Student Class
 testStudent = new Student('Bugs Bunny', 'bugs@bunny.com');
 console.log(testStudent);
 if ( testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com') {
     console.log('TASK 1: PASS');
 }
 
-//TEST OF TASK 2  - Testing Bootcamp Class
+// TEST OF TASK 2 - Bootcamp Class
 reactBootcamp = new Bootcamp("React", "Advanced");
 console.log(reactBootcamp);
 if ( reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced'
@@ -118,17 +112,8 @@ if ( reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced'
     console.log('TASK 2: PASS');
 }
 
-//TEST OF TASK 3
-// const runTest = (bootcamp, student) => {
-//     const attemptOne = bootcamp.registerStudent(student);
-//     const attemptTwo = bootcamp.registerStudent(student);
-//     const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
-//     if ( attemptOne && !attemptTwo && !attemptThree) {
-//         console.log("TASK 3: PASS");
-//     }
-// };
 
-//TEST OF TASK 4
+// TEST OF TASK 3 & 4
 const runTest = (bootcamp, student) => {
     const attemptOne = bootcamp.registerStudent(student);
     const attemptTwo = bootcamp.registerStudent(student);
@@ -141,27 +126,22 @@ const runTest = (bootcamp, student) => {
     if (bootcamp.listStudents()) {
         console.log("TASK 4: PASS 1/2");
     }
-    // bootcamp.students = [];
-    // if (!bootcamp.listStudents()) {
-    //     console.log("TASK 4: PASS 2/2");
-    // }
+    bootcamp.students = [];
+    if (!bootcamp.listStudents()) {
+        console.log("TASK 4: PASS 2/2");
+    }
 };
 
 runTest(reactBootcamp, testStudent);
 
-console.log("BONUS TASKS");
-reactBootcamp.getinfo();
-reactBootcamp.listStudentsTable();
-//test remove student
-reactBootcamp.removeStudent("bafgh@bunny.com");
-reactBootcamp.removeStudent("babs@bunny.com");
+// BONUS TASKs
+console.log("\nBONUS TASKS\nI will repopulate the bootcamp with Bugs Bunny, Babs Bunny and 10 more random students.");
 
-reactBootcamp.listStudents();
-testStudent.getinfo();
+ reactBootcamp.registerStudent(testStudent);
+ reactBootcamp.registerStudent(new Student('Babs Bunny', 'babs@bunny.com'));
 
-// TRY TO USE AN API TO FETCH A BUNCH OF RANDOM WORDS TO USE AS NEW STUDENTS BUT 
-// APPARENTLY NUCAMP HAD US INSTALLED NODE V16.13.2 AND IT IS NOT UNTIL NODE V18 
-// THAT FETCH WAS ADDED
+// I tried using an API to fetch a list of random words to use for new students but we use Node v16.13.2 
+// and 'fetch' wasn't implemented until Node v18 so I used the browser and then copy it.
 
 // async function fetchWord() {
 // 	const word = await fetch(
@@ -172,18 +152,26 @@ testStudent.getinfo();
 // fetchWord();
 
 let jsonArr = ["debased","pizzles","assegaied","stormed","typographed","pinta","bennes","edgers","waterfalls","fearing","morphallaxis","untrusses","poultryman","bethesda","perionychium","computerizes","wartier","refraction","hermes","theodicies"];
-console.log(jsonArr.length);
+
+// Create 10 random students
 for (let i = 0; i < jsonArr.length; i+=2) {
-	
 	let newName = jsonArr[i] + " " + jsonArr[i+1];
 	let newEmail = jsonArr[i] + "." + jsonArr[i+1] + "@bunny.com";
-
-	console.log(newName);
-	console.log(newEmail);
-
 	reactBootcamp.registerStudent(new Student(newName, newEmail));
-
 }
+
+console.log(`\nPRINT STUDENTS AS A TABLE`);
+reactBootcamp.listStudentsTable();
+
+console.log(`\nGET BOOTCAMP INFO`);
+reactBootcamp.getinfo();
+
+console.log(`\nGET STUDENT INFO`);
+testStudent.getinfo();
+
+console.log(`\nREMOVE STUDENT TEST`);
+reactBootcamp.removeStudent("bafgh@bunny.com");
+reactBootcamp.removeStudent("babs@bunny.com");
 
 reactBootcamp.listStudentsTable();
 
